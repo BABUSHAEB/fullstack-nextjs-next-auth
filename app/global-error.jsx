cb2b88dec50774581e0fd79a4aa1d99438081121
babushaeb <abhisheksingj123@gmail.com>
2023-09-14 18:13:42 +0530

    error page

--- app/global-error.jsx
+++ app/global-error.jsx
@@ -11,17 +11,15 @@ export default function GlobalError({ error, reset }) {
                 <div class="absolute">
                   <div class="">
                     <h1 class="my-2 text-gray-800 font-bold text-2xl">
-                      {`Looks like you've found the
-                            doorway to the great nothing`}
+                      Somthing went Wrong !
                     </h1>
                     <p class="my-2 text-gray-800">
-                      Sorry about that! Please visit our hompage to get where
-                      you need to go.
+                      Sorry about that! Please Refresh
                     </p>
 
                     <button
                       onClick={() => reset()}
-                      class="sm:w-full lg:w-auto my-2 border rounded md py-4 px-8 text-center bg-indigo-600 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-opacity-50"
+                      class="sm:w-full lg:w-auto my-2 border rounded md py-4 px-8 text-center bg-orange-600 text-white hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-opacity-50"
                     >
                       Take me there!
                     </button>
