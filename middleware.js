6f35318c9d66e2ba7f4ded12de59647b868f7038
babushaeb <abhisheksingj123@gmail.com>
2023-09-23 10:35:37 +0530

    Added iddleware

--- middleware.js
+++ middleware.js
@@ -3,6 +3,7 @@ import { NextRequest } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request) {
+  // console.log(request.nextUrl.pathname);
   if (request) {
     // const coolies = request.cookies.get("next-auth.session-token")?.value;
     const isAuthorized =
@@ -12,12 +13,13 @@ export function middleware(request) {
 
     if (
       !isAuthorized &&
-      (request.nextUrl.pathname === "/create-blog" ||
-        request.nextUrl.pathname === "/update-blog")
+      (request.nextUrl.pathname === process.env.SITE_URL + "/create-blog" ||
+        request.nextUrl.pathname === process.env.SITE_URL + "/update-blog")
     ) {
       return NextResponse.redirect(new URL("/", request.url));
     }
-    if (request.nextUrl.pathname === "/api/:path*") {
+
+    if (request.nextUrl.pathname.startsWith(process.env.SITE_URL + "/api")) {
       return NextResponse.redirect(new URL("/", request.url));
     }
   }
